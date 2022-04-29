088de265c75615c21648574e0c09d9e722dec3a8
Manuel <manuel.montilla.varo@iesjulianmarias.es>
2022-04-29 19:01:54 +0200

    V2

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -4,10 +4,20 @@
  */
 package ejercicios7.pkg2;
 
+import java.io.File;
+
 /**
  *
  * @author admin
  */
 public class Ejercicio3 {
     
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+            File f = new File("Ejercicio3.txt");
+                
+}
 }
