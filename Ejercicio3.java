635b9323e4ddcfbd2848bec76e5a6d41043222b2
Manuel <manuel.montilla.varo@iesjulianmarias.es>
2022-04-29 18:53:57 +0200

    Ejercicio 3

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ejercicios7.pkg2;
 
@@ -9,12 +9,5 @@ package ejercicios7.pkg2;
  * @author admin
  */
 public class Ejercicio3 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
 }
